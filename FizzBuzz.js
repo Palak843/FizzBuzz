033726ed28b8624f59ac6899f2cb1a1a5fe7b3ab
Palak Mittal <Palak.Mittal@softwire.com>
2022-04-26 17:48:06 +0100

    numbers print with string till multiples of 3,5,7,11,13

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,14 +4,33 @@ console.log("Hello World");
 for (var i = 1; i <=100;i++)
 {
     let resultString = '';
-    
     if (i%3 === 0)
     {
         resultString += "Fizz";
     }
+   
     if(i%5 === 0 )
     {
         resultString += "Buzz";
+    }
+    if(i%7 === 0 )
+    {
+        resultString += "Bang";
+    }
+    if(i%11 === 0 )
+    {
+        resultString = "Bong";
+    }
+    if (i%13 === 0)
+    {
+        const bIndex = resultString.indexOf("B");
+        if (bIndex === -1)
+        {
+            resultString +="Fezz";
+        }
+        else{
+            resultString = resultString.slice(0,bIndex)+ "Fezz" + resultString.slice(bIndex);
+        }
     }
     if (resultString === '')
     {
@@ -21,5 +40,12 @@ for (var i = 1; i <=100;i++)
 }
 }
 fizzBuzz();
+
+
+
+
+
+
+
 
 
