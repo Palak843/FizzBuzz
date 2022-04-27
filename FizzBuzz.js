a20209d20450ab2ac2431efa6876f05597ff5d26
Palak Mittal <Palak.Mittal@softwire.com>
2022-04-27 22:42:45 +0100

    part 3 with dictionary

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,32 +1,67 @@
 function fizzBuzz(){
 console.log("Hello World");
+
 for (var i = 1; i <=100;i++){
     let resultString = '';
+    var arr = [];
     if (i%3 === 0){
         resultString += "Fizz";
+        arr.push("Fizz");
+        //console.log(i);
     }
    if(i%5 === 0 ){
         resultString += "Buzz";
+        arr.push("Buzz");
+        //console.log(i);
     }
     if(i%7 === 0 ){
         resultString += "Bang";
+        arr.push("Bang");
+        //console.log(i);
     }
     if(i%11 === 0 ){
         resultString = "Bong";
+        arr = []
+        arr.push("Bong");
+        //console.log(i);
     }
     if (i%13 === 0){
+        
         const bIndex = resultString.indexOf("B");
+        
+
         if (bIndex === -1){
             resultString +="Fezz";
         }
         else{
             resultString = resultString.slice(0,bIndex)+ "Fezz" + resultString.slice(bIndex);
         }
+        const bIndex2 = arr.forEach(checkval)
+        function checkval(printval)
+        {
+            printval.indexOf("B");
+        }
+        if (bIndex2 === -1){
+            arr.push("Fezz");
+        }
+        else{
+            console.log(bIndex2)
+            resultString = resultString.slice(0,bIndex)+ "Fezz" + resultString.slice(bIndex);
+        }
+    }
+    if (i%17 === 0){
+        arr = arr.reverse();
+    }
+    
+    if(arr.length == 0)
+    {
+        arr.push(i)
     }
     if (resultString === ''){
         resultString += i;
     }
     console.log(resultString);
+    console.log(arr.toString());
 }
 }
 fizzBuzz();
--- emailExtract.js
+++ emailExtract.js
@@ -1,10 +1,47 @@
+//const { match } = require('assert');
 fs = require('fs')
-var stremail = ''
-
-stremail = fs.readFileSync('test.txt', 'utf8') 
+var stremail = fs.readFileSync('test.txt', 'utf8') 
 //console.log(stremail.length);
 
+//exercise 2
+//const emailRegex = /@softwire.com\b/g;
+//const matches = stremail.match(emailRegex);
+//console.log(matches);
 
-const emailRegex = /@softwire\.com\b/g;
-const matches = stremail.match(emailRegex);
-console.log(matches);
+
+const allDomainRegex = /\b(\S+)@(\S+)\b/g;
+const matches = stremail.match(allDomainRegex);
+//console.log(matches)
+
+const domainSet = new Set();
+
+matches.forEach((email) => { 
+    const Indexval = email.indexOf('@')
+    const domain = email.slice(Indexval);
+    //console.log(email.slice(Indexval));
+    domainSet.add(domain);
+})
+/*
+//matches.forEach(count);
+function count(email){
+    //console.log(email);
+    const Indexval = email.indexOf('@')
+    const domain = email.slice(Indexval);
+    //console.log(email.slice(Indexval));
+    domainSet.add(domain);
+}
+*/
+//console.log(domainSet);
+const domainCount = {}
+domainSet.forEach((domain) =>{
+    domainCount[domain] = 0;
+})
+for (let i=0;i< matches.length;i++){   
+    const email = matches[i];
+    const Indexval = email.indexOf('@');
+    const domain = email.slice(Indexval);
+
+    domainCount[domain] = domainCount[domain] + 1;
+}
+
+console.log (domainCount);
