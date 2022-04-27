201a510e6b28c36d1b1424665d48137603a3c6cb
Palak Mittal <Palak.Mittal@softwire.com>
2022-04-27 12:46:16 +0100

    read all email addresses

--- emailExtract.js
+++ emailExtract.js
@@ -1,8 +1,19 @@
 fs = require('fs')
-fs.readFile('test.txt', 'utf8', function (err,stremail) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(stremail);
-})
+var stremail = ''
 
+stremail = fs.readFileSync('test.txt', 'utf8') 
+//console.log(stremail.length);
+
+
+var counter = 0
+ 
+words_arr = stremail.split(/\s+/)
+
+for (let i=0; i<words_arr.length; i++){
+    if (words_arr[i].endsWith('@softwire.com')) {
+        console.log(words_arr[i])
+    counter++
+    }
+}
+
+console.log(counter) 
